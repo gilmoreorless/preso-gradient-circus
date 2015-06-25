8c5682be79a53e86a788dc3565922fa8a4ff7b6e
Gilmore Davidson <gilmoreorless@gmail.com>
2015-06-25 18:14:25 +1000

    Basic blending demo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,67 @@
 // Setup
-[].forEach.call(document.querySelectorAll('[data-code-colour]'), function (elem) {
+var each = Array.prototype.forEach;
+
+each.call(document.querySelectorAll('[data-code-colour]'), function (elem) {
   var colour = elem.textContent.trim();
   var preview = document.createElement('span');
   preview.className = 'code-colour-preview';
   preview.style.backgroundColor = colour;
   elem.insertBefore(preview, elem.firstChild);
 });
+
+function updateBlendDemo(stage) {
+  var colours = {
+    source: [255, 0, 0, 0.6],
+    dest: [255, 255, 255, 1],
+    output: [, , , 1]
+  };
+  var i;
+
+  stage = +stage || 0;
+  if (stage) {
+    if (stage >= 1) {
+      for (i = 0; i < 3; i++) {
+        colours.source[i] *= colours.source[3];
+      }
+    }
+    if (stage >= 2) {
+      for (i = 0; i < 4; i++) {
+        colours.dest[i] *= (1 - colours.source[3]);
+      }
+    }
+  }
+
+  for (i = 0; i < 3; i++) {
+    colours.output[i] = colours.source[i] + colours.dest[i];
+  }
+
+  var bits = ['red', 'green', 'blue', 'alpha'];
+  each.call(document.querySelectorAll('.demo-blend tr'), function (row) {
+    var bit = row.className.replace('channel-', '');
+    var bitIdx = bits.indexOf(bit);
+    if (bitIdx === -1) {
+      return;
+    }
+    each.call(row.children, function (cell) {
+      var type = cell.getAttribute('data-type');
+      if (!type) {
+        return;
+      }
+      var code = cell.querySelector('code');
+      var fill = cell.querySelector('.channel-fill');
+      var value = colours[type][bitIdx];
+      if (code) {
+        code.textContent = value;
+      }
+      if (fill) {
+        var perc = bit === 'alpha' ? value : value / 255;
+        fill.style.height = (perc * 100) + '%';
+      }
+    });
+  });
+}
+updateBlendDemo();
+
 
 // Default bespoke plugins
 var bespoke = require('bespoke'),
@@ -60,7 +116,7 @@ var showcasePlugin = function () {
 };
 
 // Bespoke.js
-bespoke.from('article', [
+var deck = bespoke.from('article', [
   classes(),
   state(),
   keys(),
@@ -76,3 +132,9 @@ bespoke.from('article', [
 require('prism');
 require('prefixfree');
 require('conic-gradient');
+
+// Custom events
+deck.on('linked-step-change', function (event) {
+  updateBlendDemo(event.step);
+});
+
--- src/scripts/plugin-linked-steps.js
+++ src/scripts/plugin-linked-steps.js
@@ -10,6 +10,10 @@ module.exports = function() {
       bullets = deck.slides.map(function(slide) {
         var steps = [];
         var stepNodes = [].slice.call(slide.querySelectorAll('[data-linked-step]'), 0);
+        var indicators = [].slice.call(slide.querySelectorAll('[data-linked-indicator]'), 0);
+        if (slide.hasAttribute('data-linked-indicator')) {
+          indicators.unshift(slide);
+        }
         stepNodes.forEach(function (step) {
           var stepNum = step.getAttribute('data-linked-step');
           if (stepNum) {
@@ -22,7 +26,7 @@ module.exports = function() {
           }
         });
         if (!steps.length) {
-          return steps;
+          return {indicators: [], steps: []};
         }
         // Re-index the possibly sparse array
         steps = steps.filter(function (s) {
@@ -32,7 +36,10 @@ module.exports = function() {
         if (steps[0].stepNum > 0) {
           steps.unshift({stepNum: 0, parts: []});
         }
-        return steps;
+        return {
+          indicators: indicators,
+          steps: steps
+        };
       }),
 
       next = function() {
@@ -62,7 +69,7 @@ module.exports = function() {
         activeStepIndex = stepIndex;
 
         bullets.forEach(function(slide, s) {
-          slide.forEach(function(step, st) {
+          slide.steps.forEach(function(step, st) {
             step.parts.forEach(function(part) {
               part.classList.add('linked-step');
 
@@ -81,11 +88,20 @@ module.exports = function() {
               }
             });
           });
+
+          slide.indicators.forEach(function(indicator) {
+            indicator.setAttribute('data-linked-current-step', stepIndex);
+          });
+        });
+
+        deck.fire('linked-step-change', {
+          slide: slideIndex,
+          step: stepIndex
         });
       },
 
       activeSlideHasBulletByOffset = function(offset) {
-        return bullets[activeSlideIndex][activeStepIndex + offset] !== undefined;
+        return bullets[activeSlideIndex].steps[activeStepIndex + offset] !== undefined;
       };
 
     deck.on('next', next);
