90737999a9b818d687d26be1b16feac372caf219
Gilmore Davidson <gilmoreorless@gmail.com>
2024-05-12 22:03:42 +1000

    Remove analytics

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -259,16 +259,3 @@ deck.on('deactivate', function (event) {
     delete swapping[event.index];
   }
 });
-
-// Analytics
-var _gaq = window._gaq = _gaq || [];
-if (~location.hostname.indexOf('github.io')) {
-  _gaq.push(['_setAccount', 'UA-8341018-3']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = 'https://ssl.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-}
